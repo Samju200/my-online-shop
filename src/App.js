2cea4ff74b980f33361b84668b68201988fb7b7b
Adeyemi juwon <samju7778@gmail.com>
2021-10-12 11:17:19 +0100

    shop-online

--- src/App.js
+++ src/App.js
@@ -1,57 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Backdrop from './components/Backdrop';
+import CartScreen from './components/CartScreen';
+import HomeScreen from './components/HomeScreen';
+import Navbar from './components/Navbar';
+import ProductScreen from './components/ProductScreen';
+import SideDrawer from './components/SideDrawer';
 
 function App() {
+  const [sideToggle, setSideToggle] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar click={() => setSideToggle(true)} />
+        <SideDrawer show={sideToggle} />
+        <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
+        <main>
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route exact path="/product/:id" component={ProductScreen} />
+            <Route exact path="/cart" component={CartScreen} />
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
