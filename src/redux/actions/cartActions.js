3558fafbef2bebb5667147d80355d34e93e90790
samju200 <samju7778@gmail.com>
2023-07-05 10:33:55 +0100

    new linkst

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -1,9 +1,9 @@
-import * as actionTypes from '../constants/cartConstants';
+import * as actionTypes from "../constants/cartConstants";
 
-import axios from 'axios';
+import axios from "axios";
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(
-    `https://foodapisamju.herokuapp.com/api/products/${id}`
+    `https://drab-puce-adder-tie.cyclic.app/api/products/${id}`
   );
   dispatch({
     type: actionTypes.ADD_TO_CART,
@@ -16,12 +16,12 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       qty,
     },
   });
-  localStorage.setItem('cart', JSON.stringify(getState().cart.cartItems));
+  localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
 };
 export const removeFromCart = (id) => async (dispatch, getState) => {
   dispatch({
     type: actionTypes.REMOVE_FROM_CART,
     payload: id,
   });
-  localStorage.setItem('cart', JSON.stringify(getState().cart.cartItems));
+  localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
 };
